58370a80f55ceb56ee6d6f9dba36c6176c1aa72d
bytecod3 <emwiti658@gmail.com>
2024-04-29 13:13:09 +0300

    adds socket init program

---
+++ networking/sockets-API/socket.c
@@ -0,0 +1,32 @@
+#if defined(_WIN32)
+#ifndef  _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#endif
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+#endif
+
+#include <stdio.h>
+
+int main() {
+#if defined (_WIN32)
+    // initialize the windows WINSOCK API
+    WSADATA d;
+    if(WSAStartup(MAKEWORD(2,2), &d) ) {
+        fprintf(stderr, "failure yo initialize\n");
+        return -1;
+    }
+#endif
+
+    printf("Ready to use sockets API!\n");
+
+#if defined(_WIN32)
+    WSACleanup();
+#endif
+
+    return 0;
+
+}
